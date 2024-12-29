b81c25bf6908ef3a79b0d7c2f1080ba0a9b574ff
HexaLaunch <141622726+ajmal-j@users.noreply.github.com>
2024-12-29 13:24:00 +0530

    chore : initialized repository with react tailwind.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='min-h-screen grid place-content-center text-3xl font-thin tracking-wider'>
+      <h3 className='flex gap-2'>
+        vite shadcn <span className='underline'>template</span>.
+      </h3>
+    </div>
+  );
+}
+
+export default App;
