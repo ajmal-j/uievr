410df5f8f959cd7a103585914f93149998b2ae00
HexaLaunch <ajmaljaleel7860@gmail.com>
2025-03-08 16:24:28 +0530

    feat : fluid scrolling effect.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReactLenis } from "lenis/react";
+import { useEffect, useRef } from "react";
+
 import Header from "./components/layouts/header";
 import Home from "./pages/home";
 
 function App() {
+  const lenisRef = useRef<any>(null);
+
+  useEffect(() => {
+    const lenis = lenisRef.current?.lenis;
+
+    if (!lenis) return;
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(raf as any);
+    };
+  }, []);
+
   return (
-    <div className='min-h-[3000px] bg-[#080716]'>
-      <Header />
-      <Home />
-    </div>
+    <ReactLenis
+      options={{
+        duration: 3,
+        smoothWheel: true,
+      }}
+      root
+      ref={lenisRef}
+    >
+      <div className="min-h-[3000px] bg-[#080716]">
+        <Header />
+        <Home />
+      </div>
+    </ReactLenis>
   );
 }
 
