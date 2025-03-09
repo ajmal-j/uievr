911df978eb62e584119328b8c3c4da9fec71e090
HexaLaunch <ajmaljaleel7860@gmail.com>
2025-03-10 00:21:03 +0530

    feat : our works parallax effect.

--- src/components/pages/home/our-works.tsx
+++ src/components/pages/home/our-works.tsx
@@ -7,6 +7,10 @@ import {
 } from "@/components/ui/carousel";
 import GradientHeading from "@/components/ui/gradient-heading";
 import GradientSubHeading from "@/components/ui/gradient-subHeading";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 
 const works = [
   {
@@ -27,6 +31,16 @@ const works = [
 ];
 
 export default function OurWorks() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const setIndex = (index: number) => {
+    if (index === activeIndex) {
+      return;
+    }
+
+    setActiveIndex(index);
+  };
+
   return (
     <div className='centered-wrapper flex flex-col sm:gap-10 gap-6' id='works'>
       <div className='flex w-full flex-col place-self-center max-w-[1240px] xl:pb-10 lg:pb-8 md:pb-6'>
@@ -34,7 +48,9 @@ export default function OurWorks() {
         <GradientHeading>How, we can produce work </GradientHeading>
       </div>
 
-      <div className='lg:grid hidden grid-cols-3 xl:gap-12 lg:gap-8 md:gap-6 sm:gap-4 gap-2'>
+      <div></div>
+
+      {/* <div className='lg:grid hidden grid-cols-3 xl:gap-12 lg:gap-8 md:gap-6 sm:gap-4 gap-2'>
         {works.map((work, index) => (
           <div key={index} className='flex flex-col gap-5 w-full'>
             <div className='flex-1'>
@@ -105,14 +121,239 @@ export default function OurWorks() {
             ))}
           </CarouselContent>
         </Carousel>
-      </div>
-      <div className='w-full flex items-center justify-center'>
+      </div> */}
+
+      <ParallaxProvider>
+        <motion.div
+          layout
+          className='w-full relative min-h-[min(100dvh,800px)]'
+        >
+          <div
+            className={
+              activeIndex === 1
+                ? "fixed inset-0 grid place-content-center bg-black/70 z-10"
+                : "z-[1]"
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setIndex(0);
+            }}
+          >
+            <Parallax speed={activeIndex === 1 ? 0 : 30}>
+              <ParalaxImage
+                setIndex={setIndex}
+                isActive={activeIndex === 1}
+                index={1}
+                className='left-[40%]'
+                path='/pages/home/images/works-parallax/1.png'
+              />
+            </Parallax>
+          </div>
+
+          <div
+            className={
+              activeIndex === 2
+                ? "fixed inset-0 grid place-content-center bg-black/70 z-10"
+                : "z-[1]"
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setIndex(0);
+            }}
+          >
+            <Parallax speed={activeIndex === 2 ? 0 : 25}>
+              <ParalaxImage
+                setIndex={setIndex}
+                isActive={activeIndex === 2}
+                index={2}
+                className='left-[0%]'
+                path='/pages/home/images/works-parallax/3.png'
+              />
+            </Parallax>
+          </div>
+
+          <div
+            className={
+              activeIndex === 3
+                ? "fixed inset-0 grid place-content-center bg-black/70 z-10"
+                : "z-[1]"
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setIndex(0);
+            }}
+          >
+            <Parallax speed={activeIndex === 3 ? 0 : 10}>
+              <ParalaxImage
+                setIndex={setIndex}
+                isActive={activeIndex === 3}
+                index={3}
+                className='left-[20%]'
+                path='/pages/home/images/works-parallax/2.png'
+              />
+            </Parallax>
+          </div>
+
+          <div
+            className={
+              activeIndex === 4
+                ? "fixed inset-0 grid place-content-center bg-black/70 z-10"
+                : "z-[1]"
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setIndex(0);
+            }}
+          >
+            <Parallax speed={activeIndex === 4 ? 0 : 20}>
+              <ParalaxImage
+                setIndex={setIndex}
+                isActive={activeIndex === 4}
+                index={4}
+                className='left-[70%]'
+                path='/pages/home/images/works-parallax/4.png'
+              />
+            </Parallax>
+          </div>
+
+          <div
+            className={
+              activeIndex === 5
+                ? "fixed inset-0 grid place-content-center bg-black/70 z-10"
+                : "z-[1]"
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setIndex(0);
+            }}
+          >
+            <Parallax speed={activeIndex === 5 ? 0 : -5}>
+              <ParalaxImage
+                setIndex={setIndex}
+                isActive={activeIndex === 5}
+                index={5}
+                className='left-[60%]'
+                path='/pages/home/images/works-parallax/5.png'
+              />
+            </Parallax>
+          </div>
+
+          <div
+            className={
+              activeIndex === 6
+                ? "fixed inset-0 grid place-content-center bg-black/70 z-10"
+                : "z-[1]"
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setIndex(0);
+            }}
+          >
+            <Parallax speed={activeIndex === 6 ? 0 : 10}>
+              <ParalaxImage
+                setIndex={setIndex}
+                isActive={activeIndex === 6}
+                index={6}
+                className='left-[68%] mt-[20%]'
+                path='/pages/home/images/works-parallax/2.png'
+              />
+            </Parallax>
+          </div>
+
+          <div
+            className={
+              activeIndex === 7
+                ? "fixed inset-0 grid place-content-center bg-black/70 z-10"
+                : "z-[1]"
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setIndex(0);
+            }}
+          >
+            <Parallax speed={activeIndex === 7 ? 0 : 10}>
+              <ParalaxImage
+                setIndex={setIndex}
+                isActive={activeIndex === 7}
+                index={7}
+                className='left-[30%] mt-[20%]'
+                path='/pages/home/images/works-parallax/5.png'
+              />
+            </Parallax>
+          </div>
+
+          <div
+            className={
+              activeIndex === 8
+                ? "fixed inset-0 grid place-content-center bg-black/70 z-10"
+                : "z-[1]"
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setIndex(0);
+            }}
+          >
+            <Parallax speed={activeIndex === 8 ? 0 : 5}>
+              <ParalaxImage
+                setIndex={setIndex}
+                isActive={activeIndex === 8}
+                index={8}
+                className='left-[5%] mt-[20%]'
+                path='/pages/home/images/works-parallax/4.png'
+              />
+            </Parallax>
+          </div>
+        </motion.div>
+      </ParallaxProvider>
+
+      {/* <div className='w-full flex items-center justify-center'>
         <button className='inline-flex items-center justify-center rounded-full bg-secondaryBackground xl:px-12 lg:px-10 md:px-8 sm:px-6 px-4 sm:py-3 py-2'>
           <span className='text-black font-urbanist leading-none text-nowrap lg:text-base md:text-sm sm:text-xs text-[10px]'>
             See All
           </span>
         </button>
-      </div>
+      </div> */}
     </div>
+  );
+}
+
+function ParalaxImage({
+  className,
+  path,
+  isActive,
+  setIndex,
+  index,
+}: {
+  className?: string;
+  path: string;
+  isActive?: boolean;
+  setIndex: (index: number) => void;
+  index: number;
+}) {
+  return (
+    <motion.img
+      layoutId={`parallax-image-${index}`}
+      onClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        setIndex(index);
+      }}
+      src={path}
+      className={cn(
+        `rounded-[11px] border-[5px] border-black max-h-[80dvh] object-contain object-top ${
+          isActive ? "block overflow-auto" : "absolute  overflow-hidden"
+        } bg-black`,
+        !isActive && className
+      )}
+      alt=''
+    ></motion.img>
   );
 }
